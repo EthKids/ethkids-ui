1f9c4977ddd6c7ac288824f74c56a68d7f794390
kirilivanov <k.ivanov@loyalgarden.co>
2019-05-28 13:47:10 +0200

    EthKids UI: AWS S3 auto deployment

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,22 @@
-module.exports = {}
+module.exports = {
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'ethkids',
+      region: 'eu-west-1',
+      bucket: 'ethkids.io',
+      createBucket: false,
+      staticHosting: false,
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: true,
+      cloudfrontId: 'EZ2HHWQELGI8Q',
+      cloudfrontMatchers: '/*',
+      uploadConcurrency: 5,
+      pluginVersion: '3.0.0'
+    }
+  }
+}
